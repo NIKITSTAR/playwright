5d483ca8396981f6219051a47ba0754d43c54121
Nikita Starodubtsev <nik.workwork@gmail.com>
2025-03-05 13:39:03 +0700

    Playwright UI тест

---
+++ pages/logout_page.py
@@ -0,0 +1,17 @@
+from pages.base_page import BasePage
+
+
+class LogoutPage(BasePage):
+    def __init__(self, page):
+        super().__init__(page)
+        self._endpoint = ''
+
+    BURGER_SELECTOR = '#react-burger-menu-btn'
+    LOGOUT_SELECTOR = '[data-test=logout-sidebar-link]'
+    LOGIN_BUTTON_SELECTOR = '#login-button'
+
+    def logout(self):
+        #self.navigate_to()
+        self.wait_for_selector_and_click(self.BURGER_SELECTOR)
+        self.wait_for_selector_and_click(self.LOGOUT_SELECTOR)
+        self.assert_element_is_visible(self.LOGIN_BUTTON_SELECTOR)
